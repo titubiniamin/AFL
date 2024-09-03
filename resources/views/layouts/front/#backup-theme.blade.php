a75b044c8673b4711bd8f24a9d74a9a04b12b10b
Bini Amin <titubiniamin@gmail.com>
2024-09-03 15:38:41 +0600

    left right fixed

---
+++ resources/views/layouts/front/#backup-theme.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.front.header')
+
+<body class="margin: 0px; padding: 0px;">
+    <div class="w-full container-xxl bg-white p-0" style="max-width: 1500px;width: 100%">
+
+        <div class="position-relative p-0">
+            @include('layouts.front.top')
+            @yield('content')
+
+        </div>
+        <!-- Navbar & Hero End -->
+
+
+        @include('layouts.front.footer')
+    </div>
+</body>
+</html>
--- resources/views/layouts/front/theme.blade.php
+++ resources/views/layouts/front/theme.blade.php
@@ -2,18 +2,33 @@
 <html lang="en">
 @include('layouts.front.header')
 
-<body class="margin: 0px; padding: 0px;">
-    <div class="w-full container-xxl bg-white p-0" style="max-width: 1600px;width: 100%">
+<body>
+<div class="container bg-gradient-light p-0">
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
 
-        <div class="position-relative p-0">
+    <!-- Main Content Start -->
+    <div class="row">
+        <!-- Left Column -->
+        <div class="col-lg-10 position-relative p-0">
             @include('layouts.front.top')
             @yield('content')
-
         </div>
-        <!-- Navbar & Hero End -->
 
-
-        @include('layouts.front.footer')
+        <!-- Right Column -->
+        <div class="col-lg-2 bg-secondary text-white p-4">
+            <h2>Right Column</h2>
+            <p>This is the right column with additional content.</p>
+        </div>
     </div>
+    <!-- Main Content End -->
+
+    @include('layouts.front.footer')
+</div>
 </body>
 </html>
